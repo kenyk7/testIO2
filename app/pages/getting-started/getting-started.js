c115b5dacbf2387170a0227002e9ee586739c068
Kenyk7 <kenykalin@gmail.com>
2016-05-20 18:01:51 -0500

    fisrt commit add slide

---
+++ app/pages/getting-started/getting-started.js
@@ -0,0 +1,28 @@
+import {Page} from 'ionic-angular';
+
+@Page({
+  templateUrl: 'build/pages/getting-started/getting-started.html'
+})
+export class GettingStartedPage {
+  constructor() {
+  	this.slides = [
+	    {
+	      title: "Welcome to the Docs!",
+	      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+	      image: "img/ica-slidebox-img-1.png",
+	    },
+	    {
+	      title: "What is Ionic?",
+	      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+	      image: "img/ica-slidebox-img-1.png",
+	    },
+	    {
+	      title: "What is Ionic Platform?",
+	      description: "The <b>Ionic Platform</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+	      image: "img/ica-slidebox-img-1.png",
+	    }
+	];
+  }
+
+  
+}
